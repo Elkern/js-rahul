ac0c286d6c95d547e2a862df71e45ecd07387dc1
Elkern <162404865+Elkern@users.noreply.github.com>
2024-05-17 06:08:11 +0000

    increment concepts

--- 01_basics/basics.js
+++ 01_basics/basics.js
@@ -1,1 +1,5 @@
-console.log("Rahul");
+"use strict";  //Treat all JS code as newer version
+let a = "Rahul"
+let b = "Shubham"
+let c = "Aditya"
+console.table([a,b,c]);
---
+++ 02_operator/operator.js
@@ -0,0 +1,11 @@
+let x = 3;
+let y = x++; //1st priority is x ki value gayi y me fir hua uska increment
+y=x;
+console.table([x,y]);
+
+let a = 3;
+let b = ++a; //1st priority is a uska increment hua or fir gaya wo b me
+console.table([a,b]);
+
+
+

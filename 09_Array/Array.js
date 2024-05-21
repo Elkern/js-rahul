056a3ee153a538aee9de8af8b76df80f7b1bab83
Elkern <162404865+Elkern@users.noreply.github.com>
2024-05-21 12:35:28 +0000

    Fundamental

--- 09_Array/Array.js
+++ 09_Array/Array.js
@@ -1,36 +1,36 @@
-// Array
+// // Array
 
-// [] -- brackets
-// () -- paranthesis
-// {} -- braces
+// // [] -- brackets
+// // () -- paranthesis
+// // {} -- braces
 
-let myArr = [0,"rahul",null,undefined,123n,Symbol('123'),true];
-// console.log(myArr.at(1)); // We give index and it returns the value
-const letters = [["a","b"], "b", "c"];
+// let myArr = [0,"rahul",null,undefined,123n,Symbol('123'),true];
+// // console.log(myArr.at(1)); // We give index and it returns the value
+// const letters = [["a","b"], "b", "c"];
 
-const alphaNumeric = letters.concat([[1],[1,2], [2, 3]]);
-// console.log(alphaNumeric); 
-const array1 = [1, 2];
-const array2 = ['a', ['b', 'c']];
-// console.log(array1.concat(...array2.pop()));
+// const alphaNumeric = letters.concat([[1],[1,2], [2, 3]]);
+// // console.log(alphaNumeric); 
+// const array1 = [1, 2];
+// const array2 = ['a', ['b', 'c']];
+// // console.log(array1.concat(...array2.pop()));
 
-//Difference between slice and splice 
+// //Difference between slice and splice 
 
-//Slice - manipulates the works on copy of the array so og don't change and last index is not included
+// //Slice - manipulates the works on copy of the array so og don't change and last index is not included
 
-// let sliceArr = myArr.slice(2/*Starting Index*/,5/*Ending Index*/);
-// console.log(sliceArr);
+// // let sliceArr = myArr.slice(2/*Starting Index*/,5/*Ending Index*/);
+// // console.log(sliceArr);
  
-//Splice - Directly change the value of array here and the number of places are decided from 2nd index
+// //Splice - Directly change the value of array here and the number of places are decided from 2nd index
 
-//let myArr = [0,"rahul",null,undefined,123n,Symbol('123'),true];
-let spliceArr = myArr.splice(2/*Starting Index*/,2/*Number of places after starting number to be consider*/);
+// //let myArr = [0,"rahul",null,undefined,123n,Symbol('123'),true];
+// let spliceArr = myArr.splice(2/*Starting Index*/,2/*Number of places after starting number to be consider*/);
 
-console.log(spliceArr); // [ null, undefined ]
-console.log(myArr); //OP [ 0, 'rahul', 123n, Symbol(123), true ]
-let spliceArr1 = myArr.splice(2,2,34/*This will be replaced by the elements which are removed*/,59);
+// console.log(spliceArr); // [ null, undefined ]
+// console.log(myArr); //OP [ 0, 'rahul', 123n, Symbol(123), true ]
+// let spliceArr1 = myArr.splice(2,2,34/*This will be replaced by the elements which are removed*/,59);
 
-console.log(spliceArr1); // [ 123n, Symbol(123) ]
-console.log(myArr); //OP [ 0, 'rahul', 34, 59, true ];
+// console.log(spliceArr1); // [ 123n, Symbol(123) ]
+// console.log(myArr); //OP [ 0, 'rahul', 34, 59, true ];
 
 
